9fe23a7d4ef15d5d4a53297e2a8f804fdb561577
aditya5432-sys <aditya.patel@ucalgary.ca>
2021-08-25 19:25:18 +0000

    Added Cors Configuration.

--- src/main/java/com/citi/hackathon/Portfolio/Manager/PortfolioManagerApplication.java
+++ src/main/java/com/citi/hackathon/Portfolio/Manager/PortfolioManagerApplication.java
@@ -2,6 +2,9 @@ package com.citi.hackathon.Portfolio.Manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PortfolioManagerApplication {
@@ -9,5 +12,16 @@ public class PortfolioManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioManagerApplication.class, args);
 	}
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
+            }
+        };
+    }
+
 
 }
